ad83e08477e7c27a36a97024b31d7a8887091420
cuongpp9 <cuongpp9@gmail.com>
2018-08-12 20:50:35 +0700

    testing

---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+
+
+// describe('Out frist test', ()=>{
+//     it('Should pass', ()=>{
+//         console.log(courseValue);
+//         expect(courseValue).to.equal("$1,000.00");
+//     });
+// });
+
+describe('Out frist test', ()=>{
+    it('Should pass', ()=>{
+        expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', ()=>{
+    it('Should say hello', (done)=>{
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function(err, window){
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal("Hello World!");
+            done();
+            window.close();
+        })
+    });
+});
